7a5c41767a1edbb79eb7570bd2e1cfe1b68345f1
iamsahilr <srajbhandari63@gmail.com>
2024-05-29 12:42:19 -0500

    navbar fixed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,9 @@
-import './Header.css'
-import sorslogo from '../../assets/sorslogo.png'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import './Header.css';
+import sorslogo from '../../assets/sorslogo.png';
+import { Navbar, Container, Nav, Form, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -34,32 +26,38 @@ const Header = () => {
 
         <Navbar expand="lg" style={{paddingLeft:'50px'}}>
             <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/" className="nav-item">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/showroom" className="nav-item">About</Nav.Link>
-                        <Nav.Link as={Link} to="/products" className="nav-item">Contact</Nav.Link>
-                        <Nav.Link as={Link} to="/about-us" className="nav-item">Our Team</Nav.Link>
-                        <Nav.Link as={Link} to="/contact-us" className="nav-item">Listing</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                <Form inline>
-                    <Row>
-                        <Col xs="auto">
-                            <Form.Control
-                            type="text"
-                            placeholder="Search"
-                            className=" mr-sm-2 searchbox"
-                            />
-                        </Col>
-                        <Col xs="auto">
-                            <Button className='searchbuttonhome' type="submit">
-                                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                            </Button>
-                        </Col>
-                    </Row>
-                </Form>
+                <div>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/" className="nav-item">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/about" className="nav-item">About</Nav.Link>
+                            <Nav.Link as={Link} to="/contact" className="nav-item">Contact</Nav.Link>
+                            <Nav.Link as={Link} to="/team" className="nav-item">Our Team</Nav.Link>
+                            <Nav.Link as={Link} to="/listings" className="nav-item">Listing</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
+
+                <div>
+                    <Form inline>
+                        <Row>
+                            <Col xs="auto">
+                                <Form.Control
+                                type="text"
+                                placeholder="Search"
+                                className=" mr-sm-2 searchbox"
+                                />
+                            </Col>
+                            <Col xs="auto">
+                                <Button className='searchbuttonhome' type="submit">
+                                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </div>
+
             </Container>
             
         </Navbar>
