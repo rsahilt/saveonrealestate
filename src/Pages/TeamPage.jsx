b2678d185b78fa8d7b34e806505b7eebbb221881
iamsahilr <srajbhandari63@gmail.com>
2024-05-31 16:48:43 -0500

    team page added

--- src/Pages/TeamPage.jsx
+++ src/Pages/TeamPage.jsx
@@ -2,10 +2,77 @@ import './TeamPage.css'
 import Header from '../Components/Header/Header'
 import Footer from "../Components/Footer/Footer"
 
+import { Card, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import realtor1 from '../assets/realtor1.png'
+
 const TeamPage = () => {
   return (
     <>
         <Header />
+
+        <section className='team-section'>
+          <div>
+            <h1 className='realtor-heading'>Our Team</h1>
+          </div>
+          <div className="card-container">
+            <Card style={{ width: '500px', borderRadius:'0' }} className='realtor-card'>
+              <Row noGutters>
+                <Col md={4}>
+                  <Card.Img src={realtor1} alt="Fateh Rehsi" style={{height:'100%', borderRadius:'0'}}/>
+                </Col>
+                <Col md={8}>
+                  <Card.Body>
+                    <Card.Title style={{textTransform:'uppercase', letterSpacing:'1px',padding:'0', margin:'0'}}><strong>Fateh Rehsi</strong></Card.Title>
+                    <Card.Text style={{padding:'0', margin:'0'}}>Realtor</Card.Text>
+                    <Card.Text style={{paddingTop:'5px'}}><strong>Licence No.:</strong>
+                      <br /><strong>Contact No.:</strong>
+                      <br /><strong>Office Address:</strong>
+                    </Card.Text>
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
+
+            <Card style={{ width: '500px', borderRadius:'0' }} className='realtor-card'>
+              <Row noGutters>
+                <Col md={4}>
+                  <Card.Img src={realtor1} alt="Fateh Rehsi" style={{height:'100%', borderRadius:'0'}}/>
+                </Col>
+                <Col md={8}>
+                  <Card.Body>
+                    <Card.Title style={{textTransform:'uppercase', letterSpacing:'1px',padding:'0', margin:'0'}}><strong>Fateh Rehsi</strong></Card.Title>
+                    <Card.Text style={{padding:'0', margin:'0'}}>Realtor</Card.Text>
+                    <Card.Text style={{paddingTop:'5px'}}><strong>Licence No.:</strong>
+                      <br /><strong>Contact No.:</strong>
+                      <br /><strong>Office Address:</strong>
+                    </Card.Text>
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
+
+            <Card style={{ width: '500px', borderRadius:'0' }} className='realtor-card'>
+              <Row noGutters>
+                <Col md={4}>
+                  <Card.Img src={realtor1} alt="Fateh Rehsi" style={{height:'100%', borderRadius:'0'}}/>
+                </Col>
+                <Col md={8}>
+                  <Card.Body>
+                    <Card.Title style={{textTransform:'uppercase', letterSpacing:'1px',padding:'0', margin:'0'}}><strong>Fateh Rehsi</strong></Card.Title>
+                    <Card.Text style={{padding:'0', margin:'0'}}>Realtor</Card.Text>
+                    <Card.Text style={{paddingTop:'5px'}}><strong>Licence No.:</strong>
+                      <br /><strong>Contact No.:</strong>
+                      <br /><strong>Office Address:</strong>
+                    </Card.Text>
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
+          </div>
+        </section>
+
         <Footer />
     </>
   )
