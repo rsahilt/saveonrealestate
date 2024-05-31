9d6a2347a1ae52cf7c2c799a7263c834643c556d
iamsahilr <srajbhandari63@gmail.com>
2024-05-31 16:11:09 -0500

    contact form added

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -7,6 +7,7 @@ const AboutPage = () => {
     <>
         <Header />
 
+
         <Footer />
 
     </>
--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,11 +1,41 @@
 import './ContactPage.css'
 import Header from '../Components/Header/Header'
 import Footer from "../Components/Footer/Footer"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 const ContactPage = () => {
   return (
     <>
         <Header />
+
+        <section className="contact-section">
+          <h1 className='feedback-heading'>Get In Touch</h1>
+          <p className='feedback-span'>Send us your message, feedback or ask us your queries</p>
+          <Form className='feedback-form'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control type="text" placeholder="Full Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type="email" placeholder="Email Address" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Send us your message</Form.Label>
+              <Form.Control as="textarea" rows={5} placeholder="Enter your message here"/>
+            </Form.Group>
+
+            
+            <Button variant="primary" type="submit">
+              Send Message
+            </Button>
+          </Form>
+
+        </section>
 
         <Footer />
     </>
