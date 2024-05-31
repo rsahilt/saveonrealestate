cbeec794f44250f00bb1ddd9c419cd17545dc4be
iamsahilr <srajbhandari63@gmail.com>
2024-05-31 17:22:35 -0500

    bg colors fixed

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -11,31 +11,33 @@ const ContactPage = () => {
         <Header />
 
         <section className="contact-section">
-          <h1 className='feedback-heading'>Get In Touch</h1>
-          <p className='feedback-span'>Send us your message, feedback or ask us your queries</p>
-          <Form className='feedback-form'>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control type="text" placeholder="Full Name" />
-            </Form.Group>
+          <div className='contact-form-container'>
+            <h1 className='feedback-heading'>Get In Touch</h1>
+            <p className='feedback-span'>Send us your message, feedback or ask us your queries</p>
+            <Form className='feedback-form'>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" placeholder="Full Name" />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Contact Number</Form.Label>
-              <Form.Control type="text" placeholder="Contact Number" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Contact Number</Form.Label>
+                <Form.Control type="text" placeholder="Contact Number" />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Send us your message</Form.Label>
-              <Form.Control as="textarea" rows={5} placeholder="Enter your message here"/>
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Send us your message</Form.Label>
+                <Form.Control as="textarea" rows={5} placeholder="Enter your message here"/>
+              </Form.Group>
 
-            
-            <Button variant="primary" type="submit">
-              Send Message
-            </Button>
-          </Form>
-
-        </section>
+              
+              <Button variant="primary" type="submit">
+                Send Message
+              </Button>
+            </Form>
+            </div>
+          </section>
+        
 
         <Footer />
     </>
