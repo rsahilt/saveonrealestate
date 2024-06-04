1dcee8c1e239105f30c4842b0cf90a4e59edc038
iamsahilr <srajbhandari63@gmail.com>
2024-06-04 14:37:36 -0500

    resuming work

--- src/Components/Admin/AdminSidebar.jsx
+++ src/Components/Admin/AdminSidebar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faTachometer, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { Nav } from 'react-bootstrap';
+import { Nav, Dropdown, Button } from 'react-bootstrap';
 import './Admin.css';
 
-function Sidebar() {
+function AdminSidebar() {
   return (
     <div className='admin-sidebar'>
       <Nav className="flex-column admin-sidebar-list">
@@ -12,17 +12,37 @@ function Sidebar() {
           <Nav.Link href="#" className="admin-link text-black">
             <FontAwesomeIcon icon={faTachometer} className="me-2 admin-faicon" /> Dashboard
           </Nav.Link>
+          
         </Nav.Item>
+
         <Nav.Item>
-          <Nav.Link href="#" className="admin-link text-black">
-            <FontAwesomeIcon icon={faHome} className="me-2 admin-faicon" /> Listings
-          </Nav.Link>
+          <Dropdown>
+            <Dropdown.Toggle variant="link" style={{textDecoration:'none'}} id="dropdown-basic" className="admin-link text-black">
+              <FontAwesomeIcon icon={faHome} className="me-2 admin-faicon" /> Listings
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#action/1">View All Listing</Dropdown.Item>
+              <Dropdown.Item href="#action/2">Create New Listing</Dropdown.Item>
+              <Dropdown.Item href="#action/3">Update/Delete Listing</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Nav.Item>
+
         <Nav.Item>
-          <Nav.Link href="#" className="admin-link text-black">
-            <FontAwesomeIcon icon={faUser} className="me-2 admin-faicon" /> Realtors
-          </Nav.Link>
+          <Dropdown>
+              <Dropdown.Toggle variant="link" style={{textDecoration:'none'}} id="dropdown-basic" className="admin-link text-black">
+                <FontAwesomeIcon icon={faHome} className="me-2 admin-faicon" /> Realtors
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#action/1">View All Realtors</Dropdown.Item>
+                <Dropdown.Item href="#action/2">Add New Realtor</Dropdown.Item>
+                <Dropdown.Item href="#action/3">Edit/Remove Realtor</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
         </Nav.Item>
+        
         <Nav.Item>
           <Nav.Link href="#" className="admin-link text-black">
             <FontAwesomeIcon icon={faEnvelope} className="me-2 admin-faicon" /> Inbox
@@ -33,4 +53,4 @@ function Sidebar() {
   );
 }
 
-export default Sidebar;
+export default AdminSidebar;
--- src/Pages/Admin/Dashboard.jsx
+++ src/Pages/Admin/Dashboard.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import AdminNavbar from '../../Components/Admin/AdminNavbar'
 import Sidebar from '../../Components/Admin/AdminSidebar'
 import './Dashboard.css'
+import Card from 'react-bootstrap/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
 
 const Dashboard = () => {
@@ -14,7 +17,38 @@ const Dashboard = () => {
             <Sidebar />
           </div>
 
-          <div className="dashboard-content"></div>
+          <div className="dashboard-content">
+            <h1 style={{fontWeight:'500', fontSize:'1.5em', padding:'10px'}}>Welcome to Dashboard</h1>
+
+            <div className="admin-summary">
+              <Card style={{ width: '18rem', marginRight:'25px' }}>
+                <Card.Body>
+                  <Card.Title>Listings</Card.Title>
+                  <Card.Text>
+                    <FontAwesomeIcon icon={faHome} style={{fontSize: '2em'}}/>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+
+              <Card style={{ width: '18rem', marginRight:'25px' }}>
+                <Card.Body>
+                  <Card.Title>Realtors</Card.Title>
+                  <Card.Text>
+                    <FontAwesomeIcon icon={faUser} style={{fontSize: '2em'}}/>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+
+              <Card style={{ width: '18rem', marginRight:'25px' }}>
+                <Card.Body>
+                  <Card.Title>Feedbacks</Card.Title>
+                  <Card.Text>
+                    <FontAwesomeIcon icon={faUser} style={{fontSize: '2em'}}/>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
         </section>
 
     </>
